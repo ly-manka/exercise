9a21bcc97ec26e274e63d3fd3c792e6cb8e6db8d
iryna <goiralysko@gmail.com>
2023-07-09 14:07:23 +0300

    Task 1

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
+"use strict";
 
-console.log('Hello world');
+//Task 1
+
+//Hourly payment is 80$, working day is 5/2 5 hours. Order 40 hours.
+//Vacation is planned in 11 days. Today is monday
+//Output boolean variable to the console if you can take the order
+//How much does the project cost?
+
+const hourlyPayment = 80;
+const workingHours = 5;
+const orderHours = 40;
+const leftDays = 9;
+
+const daysForProjectCompletion = orderHours / workingHours;
+
+if (leftDays >= daysForProjectCompletion) {
+  console.log(
+    true,
+    `Can take the project, it takes ${daysForProjectCompletion} days to complete the order and I have ${leftDays} days left`
+  );
+  console.log(`It costs ${orderHours * hourlyPayment}`);
+} else {
+  console.log(
+    false,
+    `Can't finish the project, cause it takes ${daysForProjectCompletion} and I have only ${leftDays} days left`
+  );
+}
